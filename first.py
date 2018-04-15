5b125e2b7efaf5af309d63bc3a199c263b988a17
yougun435 <yugandhar0205@gmail.com>
2018-04-15 14:49:12 +0530

    modified the code

--- first.py
+++ first.py
@@ -1,32 +1,51 @@
 from flask import Flask, send_from_directory, request, Response
 from pymongo import MongoClient
+import re
 
 client = MongoClient('localhost',27017)
 db = client.miniamazonDB
 
-
 app = Flask("mini-amazon", static_url_path="");
 
-indexPosition = 0
 @app.route('/', methods=['GET'])
 def index():
-    return send_from_directory('staticHtmlFiles',"index.html")
+    return send_from_directory('static', 'index.html')
 
-
-
-@app.route('/products', methods=['POST','GET'])
+@app.route('/products',methods=['POST','GET'])
 def addProducts():
 
     if request.method == 'POST':
-        prod = {}
-        prod['product name'] = request.form['name'];
-        prod['product description'] = request.form['desc'];
-        prod['cost'] = request.form['cost'];
-        db.products.insert(prod)
-        return Response("Product with details " + str(prod) + "successfully added........")
+        if request.form['op_type'] == "insert":
+            prod = {}
+            prod['product name'] = request.form['name'];
+            prod['product description'] = request.form['desc'];
+            prod['cost'] = request.form['cost'];
+            db.products.insert(prod)
+            return Response("Product with details " + str(prod) + "successfully added........")
+
+        elif request.form['op_type'] == "delete":
+            result = db.products.delete_one(filter = {'product name' : request.form['name']})
+            return Response("Product with name \t" + str(request.form['name']) + " \t and count: " + str(result.deleted_count) + " successfully deleted........")
+        elif request.form['op_type']=='update':
+            condition=dict()
+            condition['name'] =  request.form['cur_name']
+
+            update =dict()
+            if request.form['name'] != '':
+                update['product name']=request.form['name']
+            if request.form['description'] != '':
+                update['product description']=request.form['description']
+            if request.form['cost'] !='':
+                update['cost']=request.form['cost']
+
+            db.products.update_one(filter=condition, update={'$set: update'})
+
+
+
+
 
     elif request.method == 'GET':
-        matching_items = db.products.find({'product name': {"$regex": request.args['name']}})
+        matching_items = db.products.find({'product name': re.compile(request.args['name'], re.IGNORECASE)})
 
         if matching_items.count() <=0:
             return Response("Sorry!!! The product you searched is not found")
@@ -37,19 +56,6 @@ def addProducts():
 
 
 
-
-@app.route('/listProducts', methods=['GET'])
-def listProducts():
-    count = 0
-    allItems = db.products.find()
-    if allItems.count() > 0:
-        matches = []
-        for item in allItems:
-            matches.append(item)
-        return Response("Product details are " + str(matches) , mimetype="application/json" )
-    else:
-        return Response("Empty List! No data found.", mimetype="text/plain")
-
-
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+
+ app.run(host='0.0.0.0', port=5000, debug = True)
